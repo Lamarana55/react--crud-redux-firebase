f3286e9a684d324ca98116748bab2c0b170bd650
MAMADOU LAMARANA DIALLO <lamarana55@gmail.com>
2022-09-09 18:27:00 +0000

    work add user

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,34 +1,39 @@
 import React, { useRef } from 'react';
-import { auth } from '../utils/firebase.config.js';
+import { useState } from 'react';
+import { auth } from '../utils/firebase.config';
 
 const SignUp = () => {
     const registerEmail = useRef();
     const registerPassword = useRef();
+    const [displayName, setdisplayName] = useState("");
 
     const handleRegister = (e) =>{
-        console.log(e);
         e.preventDefault();
         try {
-            console.log("###################")
             auth.createUserWithEmailAndPassword(
                 registerEmail.current.value, 
-                registerPassword.current.value);
+                registerPassword.current.value)
+                .then(async (userAuth) => {
+                    await userAuth.user.updateProfile({
+                        displayName
+                    })
+                })
+                
         } catch (error) {
             console.log(error.message());
         }
         
     }
 
-
     return (
         <div className='signup-container'>
             <div className="signup">
                 <h3>S'inscrire</h3>
                 <form onSubmit={e => handleRegister(e)}>
-                    <input type="text" id="pseudo" required placeholder='Pseudo' />
+                    <input type="text" id="pseudo" required placeholder='Pseudo' onChange={(e)=> {setdisplayName(e.target.value)}}/>
                     <input type='email' id="email" required placeholder='email' ref={registerEmail} />
                     <input type='password' id="password" required placeholder='mot de passe' ref={registerPassword} />
-                    <input type="submit" value="Valider"/>
+                    <input type="submit" value="Valider"/> 
                 </form>
             </div>
         </div>
